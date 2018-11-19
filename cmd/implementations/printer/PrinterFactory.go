adf2dbf21794b216337eef4e40848e2d843e2298
Frank Taubert <frank.taubert@uni-jena.de>
2018-11-19 11:04:57 +0100

    fix warning

--- cmd/implementations/printer/PrinterFactory.go
+++ cmd/implementations/printer/PrinterFactory.go
@@ -1,14 +1,13 @@
 package factories
 
 import (
-	inf "github.com/FrankT1983/GoPlayGround/cmd/interface"
 	impl "github.com/FrankT1983/GoPlayGround/cmd/implementations/printer/impls"
+	inf "github.com/FrankT1983/GoPlayGround/cmd/interface"
 )
-
 
 // BuildLocalPrinter returns an implementation of the IPrinter interface which runs localy
 // on the same thread.
 func BuildLocalPrinter() inf.IPrinter {
-	result := impl.LogPrinter{"foo"}
-	return result;
+	result := impl.LogPrinter{LoggerName: "foo"}
+	return result
 }

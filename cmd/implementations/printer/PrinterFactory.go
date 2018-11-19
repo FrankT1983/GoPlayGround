b36fbf8bde5036246a169eaf026105a3f9097073
Frank Taubert <frank.taubert@uni-jena.de>
2018-11-19 10:29:24 +0100

    trying to figure out correct include and return syntax

--- cmd/implementations/printer/PrinterFactory.go
+++ cmd/implementations/printer/PrinterFactory.go
@@ -1,12 +1,13 @@
 package factories
 
 import (
-	"github.com/FrankT1983/GoPlayGround/cmd/interface"
-	"github.com/FrankT1983/GoPlayGround/cmd/implementations/printer/LogPrinter"
+	inf "github.com/FrankT1983/GoPlayGround/cmd/interface"
+	impl "github.com/FrankT1983/GoPlayGround/cmd/implementations/printer/LogPrinter"
 )
 
 // BuildLocalPrinter returns an implementation of the IPrinter interface which runs localy
 // on the same thread.
-func BuildLocalPrinter() IPrinterIPrinter {
-	return factory.LogPrinter("foo.log")
+func BuildLocalPrinter() inf.IPrinter {
+	result := impl.LogPrinter();
+	return result;
 }

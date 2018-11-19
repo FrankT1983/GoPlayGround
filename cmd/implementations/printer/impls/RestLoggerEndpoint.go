f0a0fa7b172ac453d85cf31231602e737c476154
Frank Taubert <frank.taubert@uni-jena.de>
2018-11-19 17:25:09 +0100

    Use Reflection to bind the handlers

--- cmd/implementations/printer/impls/RestLoggerEndpoint.go
+++ cmd/implementations/printer/impls/RestLoggerEndpoint.go
@@ -18,34 +18,34 @@ type RestLoggerEndpoint struct {
 // PrintToLog implements the IPrinter interface function.
 func (l RestLoggerEndpoint) PrintToLog(toPrint string) {
 	// send rest request
-	help(toPrint)
+	callStringFunctionOferRest("PrintToLog", toPrint)
 }
 
-func help(toPrint string) ([]byte, error) {
+func callStringFunctionOferRest(functionName string, toSend string) {
 	fmt.Println("start building request")
 	e := eater.NewEater("127.0.0.1", 9443)
 	e.SetBasicAuth("testuser", "testpass")
 	e.SetVerifyTLS(false)
 
-	req := e.CreateRequest("/Print", "POST", url.Values{"data": {toPrint}})
+	req := e.CreateRequest("/"+functionName, "POST", url.Values{"data": {toSend}})
 	resp, err := req.Go()
 	if err != nil {
 
 		fmt.Println("ups ... error " + string(err.Error()))
-		return nil, err
+		return
 	}
 
 	if resp.StatusCode == http.StatusOK {
 		b, err := ioutil.ReadAll(resp.Body)
 		if err != nil {
-			return nil, err
+			return
 		}
 		if strings.Contains(string(b), "error") {
 
 		}
-		return b, nil
+		return
 	}
 
 	fmt.Printf("statusCode: %d - %s\n", resp.StatusCode, http.StatusText(resp.StatusCode))
-	return nil, fmt.Errorf("statusCode: %d - %s", resp.StatusCode, http.StatusText(resp.StatusCode))
+	return
 }

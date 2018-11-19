810fbdb6b5c5dcb7678cd7d21a558907746f7956
Frank Taubert <frank.taubert@uni-jena.de>
2018-11-19 17:46:59 +0100

    use refactored struct

--- cmd/main.go
+++ cmd/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	printerFactory "github.com/FrankT1983/GoPlayGround/cmd/implementations/printer"
+	restHelper "github.com/FrankT1983/GoPlayGround/restHelper"
 )
 
 func main() {
@@ -11,8 +12,12 @@ func main() {
 	printer := printerFactory.BuildLocalPrinter()
 	printer.PrintToLog("Foo")
 
+	serverInfo := restHelper.ServerInfo{
+		Server:   "127.0.0.1",
+		Port:     9443,
+		User:     "testuser",
+		Password: "testpass"}
 	fmt.Println("Print 2")
-	printer2 := printerFactory.BuildRestPrinter()
+	printer2 := printerFactory.BuildRestPrinter(serverInfo)
 	printer2.PrintToLog("Foo over rest")
-
 }
